ed91f1236585b979dabf4118bb5626d3d2d1b258
Endomou <75765110+Endomou@users.noreply.github.com>
2023-03-24 16:29:04 +0800

    Login and register is now working

---
+++ Account.py
@@ -0,0 +1,72 @@
+import maskpass
+
+
+
+def SignIn():
+    '''function that accepts a username and a password
+    and signs in if the password is correct and exits the program
+    after 5 incorrect attempts
+    '''
+    attempt = 0
+    while attempt<5:
+        UsernameIsPresent = False
+        Username = input("Enter your Username: ")
+
+        Password = maskpass.askpass(prompt="Password:", mask="*")
+        file=open("Accounts.txt","r")
+        file_read=file.readlines()
+        #THIS IS NOT DONE
+        #check if username is present then check the password after
+        
+        for i in file_read:
+            print(i)
+            #Username is not case sensitive, Password is case sensitive
+            if "."+Username.lower()+" " in i.lower() and ","+Password+" " in i:
+                UsernameIsPresent = True
+                break
+        if UsernameIsPresent == True:
+            file.close()
+            return Username
+        elif attempt >= 0 and attempt < 4:
+            print("Invalid Username or Password, Please Try again")
+            attempt+=1
+            file.close()
+        else:
+            print("Too many attempts, Please Try again later")
+            exit()
+
+def SignUp():
+    """
+    function that creates a new account and adds it into account.txt
+    and returns the username
+    the function will not accept existing usernames
+    """
+    while True:
+        UsernameIsTaken = False
+        Username=input("Create your username:")
+        #read from database if username exists
+        
+        file=open("Accounts.txt","r")
+        file_read=file.readlines()
+        #loop through the database and check if username exists
+        for i in file_read:
+            if "."+Username.lower()+" " in i.lower():
+                UsernameIsTaken = True
+                break
+        if UsernameIsTaken==True:
+            print("Username is already taken, Please try again")
+            file.close()
+            continue
+        file.close()
+        Password = maskpass.askpass(prompt="Create your Password:", mask="*")
+        ConfirmPassword = maskpass.askpass(prompt="Confirm your Password:", mask="*")
+        if Password == ConfirmPassword:
+            file=open("Accounts.txt","a")
+            file.write("."+Username + " ," + Password +" \n")
+            print("Account created successfully")
+            file.close()
+            #append to Accounts.txt
+            return Username
+        else:
+            print("Password does not match, Please try again")
+            continue
--- main.py
+++ main.py
@@ -2,39 +2,7 @@
 when working on a feature, make sure it properly works before adding here
 """
 
-
-import maskpass
-dbUsername = "john"
-dbPassword = "johndoe123"
-UserName=""
-Password=""
-def SignIn():
-    '''function that accepts a username and a password
-    and signs in if the password is correct and exits the program
-    after 5 incorrect attempts
-    '''
-    Username = input("Enter your Username: ")
-    Password = maskpass.askpass(prompt="Password:", mask="*")
-    attempt = 0
-    while attempt<5:
-        if Username.lower() == dbUsername.lower() and Password == dbPassword :
-            print("Welcome " + Username )
-        elif attempt > 0 and attempt < 4:
-            print("Incorrect Password, Please Try again")
-            attempt+=1
-        else:
-            print("Too many attempts, Please Try again later")
-            break
-def SignUp():
-    UserName=input("Create your username:")
-    while Password!=ConfirmPassword:
-        Password = maskpass.askpass(prompt="Create your Password:", mask="*")
-        ConfirmPassword = maskpass.askpass(prompt="Confirm your Password:", mask="*")
-    
-
-
-
-
+from Account import SignIn,SignUp
 #main function
 print("Log in or Sign up First before accessing the system:")
 print("1 - Login")
@@ -42,12 +10,11 @@ print("2 - Signup")
 #Login or Sign up Part
 LogIn=input("")
 if LogIn=='1':
-    SignIn()
+    Username=SignIn()
 elif LogIn=='2':
-    SignUp()
-
-
-print("Welcome " + UserName)
+    Username=SignUp()
+    
+print("Welcome, "+ Username)
 
 print("Would you like to:")
 print("1 - Go shopping")
@@ -55,3 +22,4 @@ print("2 - Check your cart")
 print("3 - Check your orders")
 print("4 - Check your account details")
 print("5 - Log out")
+shopping_choice=input("")
